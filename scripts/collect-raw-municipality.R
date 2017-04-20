70146236bdbd9a3dd6790ea2a458480fe849a8fc
Oscar de León <odeleongt@gmail.com>
2017-04-20 16:05:44 -0600

    Collect 2000-2010 municipality data

--- scripts/collect-raw-municipality.R
+++ scripts/collect-raw-municipality.R
@@ -33,6 +33,106 @@ file_list <- unzip(zipfile = zip_path, list = TRUE) %>%
 unzip(zipfile = zip_path, exdir = temp_path)
 
 
+#------------------------------------------------------------------------------*
+# funtion to read files
+#------------------------------------------------------------------------------*
+read_population_2000 <- function(file_path, sheet = "Población TOTAL", skip = 0){
+  # Get department name from file name
+  department <- gsub(
+    "(^| )([a-z])", "\\1\\U\\2",
+    gsub("(^/([^/]+/)+)|([0-9]+ )|([^a-z .])|(.xls$)", "", tolower(file_path)),
+    perl = TRUE
+  )
+  
+  # Report current department
+  cat("\nReading: ", department, "\n", sep = "")
+  
+  # Read file contents
+  pop_file <- read_excel(
+    path = file_path, sheet = sheet, skip = skip , na = "?"
+  )
+  
+  # Get municipality names
+  municipalities <- pop_file %>%
+    slice(1) %>%
+    unlist %>%
+    na.omit %>%
+    setdiff(0) %>%
+    rep(each = 3)
+  
+  # Get first year
+  first_year <- names(pop_file)[1]
+  
+  # Get column group names
+  column_group_names <- c(
+    "",
+    rep(
+      c(":Total", ":Masculino", ":Femenino"),
+      times = (length(municipalities) / 3) + 1
+    )
+  )
+  
+  # Set variable names
+  pop_file <- pop_file %>%
+    # Select useful columns and rows
+    select(seq(1, 4 + length(municipalities))) %>%
+    slice(-1) %>%
+    # Fix names
+    set_names(
+      c("age", rep(names(.)[2], 3), municipalities)
+    ) %>%
+    set_names(
+      paste0(names(.), column_group_names)
+    ) %>%
+    set_names(
+      gsub(" +", "_", tolower(iconv(names(.), to = "ASCII//TRANSLIT")))
+    ) %>%
+    # Set first year
+    mutate(age = c(first_year, age[-1])) %>%
+    # Fix age variables and extract years
+    mutate(age = as.integer(gsub("['+]", "", age))) %>%
+    filter(!is.na(age)) %>%
+    mutate(
+      group = stats::filter(age > 100, 1, "recursive")
+    ) %>%
+    group_by(group) %>%
+    mutate(year = first(age)) %>%
+    ungroup %>%
+    filter(age < 100) %>%
+    mutate(
+      year = ifelse(year < 100, min(year[year > 100]) - 1, year)
+    ) %>%
+    gather(key = variable, value = population, -year, -age, -group) %>%
+    separate(variable, into = c("municipality", "sex"), sep = ":") %>%
+    filter(sex != "total", municipality != "total") %>%
+    # Tag with department
+    mutate(
+      municipality = gsub(
+        "(^| )([a-z])", "\\1\\U\\2",
+        gsub("_", " ", municipality),
+        perl = TRUE
+      ),
+      department = department
+    ) %>%
+    select(year, department, municipality, sex, age, population) %>%
+    mutate(
+      # Fix factors
+      sex = recode(
+        sex,
+        masculino = "male",
+        femenino = "female"
+      ),
+      population = as.numeric(population)
+    )
+  
+  # Return data
+  return(pop_file)
+}
+
+# Read in all files
+pop_2000_2010 <- lapply(file_list, read_population_2000) %>%
+  bind_rows()
+
 # Remove files
 file.remove(file_list)
 
