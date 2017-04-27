c9974fc860117c21ae414f72189a910df1ae6c38
Oscar de León <odeleongt@gmail.com>
2017-04-27 15:17:25 -0600

    Setup vital statistics

---
+++ scripts/collect-raw-vital-stats.R
@@ -0,0 +1,20 @@
+#------------------------------------------------------------------------------*
+# Collect raw vital statistics at the department level
+#------------------------------------------------------------------------------*
+
+#------------------------------------------------------------------------------*
+# Prepare environment ----
+#------------------------------------------------------------------------------*
+
+# Load used packages
+library(package = "lubridate")
+library(package = "haven")
+library(package = "tidyverse")
+
+# Set metadata
+data_path <- "data/raw/vital-statistics/"
+
+
+
+
+# End of script
