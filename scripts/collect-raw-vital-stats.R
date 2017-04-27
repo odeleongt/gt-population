69c863b5d41c4a3e1e3a55419e93729aa2a5d469
Oscar de León <odeleongt@gmail.com>
2017-04-27 16:09:50 -0600

    Prepare deaths data

--- scripts/collect-raw-vital-stats.R
+++ scripts/collect-raw-vital-stats.R
@@ -67,4 +67,69 @@ births <- births %>%
 
 
 
+#------------------------------------------------------------------------------*
+# Load deaths data ----
+#------------------------------------------------------------------------------*
+
+# Read labeled data
+deaths <- list.files(path = data_path, pattern = "deaths") %>%
+  paste0(data_path, .) %>%
+  set_names(gsub("[^0-9]", "", .)) %>%
+  map(read_spss) %>%
+  map(~set_names(.x, tolower(iconv(names(.x), to = "ASCII//TRANSLIT"))))
+
+# Read labels from imported data
+death_labels <- deaths %>%
+  map(~map(.x, ~attr(.x, "labels")))
+
+# Bind imported datasets
+deaths <- deaths %>%
+  map(~map_df(.x, zap_labels)) %>%
+  map(~map_df(.x, as.character)) %>%
+  bind_rows(.id = "file_year")
+
+# Prepare birth "events" dataset
+deaths <- deaths %>%
+  # Ignore unknown ages
+  filter(
+    as.integer(edadif) < 999,
+    perdif != "9"
+  ) %>%
+  mutate(
+    record_date = ymd(
+      paste(
+        stringr::str_pad(anoreg, width = 2, side = "left", pad = "0"),
+        mesreg, "01", sep = "-"
+      )
+    ),
+    anoocu = ifelse(is.na(anoocu), file_year, anoocu),
+    event_date = ymd(
+      paste(
+        stringr::str_pad(anoocu, width = 2, side = "left", pad = "0"),
+        mesocu, diaocu, sep = "-"
+      )
+    ),
+    age_unit = recode(
+      perdif,
+      "1" = "days",
+      "2" = "months",
+      "3" = "years",
+      .default = NA_character_,
+      .missing = NA_character_
+    ),
+    birth_date = event_date - period(edadif, units = age_unit),
+    age_days = as.integer(event_date - birth_date)
+  ) %>%
+  select(
+    # Record metadata
+    record_date, record_department = depreg, record_municipality = mupreg,
+    # Event data
+    event_date, event_department = depocu, event_municipality = mupocu,
+    # Deceased data
+    birth_date, age_days
+  )
+
+
+
+
 # End of script
